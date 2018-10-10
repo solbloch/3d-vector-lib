92469a47ea6d8a5c3e698ff20979feb5ea0a7c4a
PNGray <35545554+PNGray@users.noreply.github.com>
2018-10-09 22:21:55 -0400

    minor update to state.lisp

--- state.lisp
+++ state.lisp
@@ -1,13 +1,13 @@
 (in-package :state3d)
 
+(declaim (inline make-state state-pos state-vel state-acc state-mass))
+(declaim (inline dist))
+
 (defstruct state
   (pos (v3 0d0 0d0 0d0) :type v3)
   (vel (v3 0d0 0d0 0d0) :type v3)
   (acc (v3 0d0 0d0 0d0) :type v3)
   (mass 1d0 :type double-float))
-
-(declaim (inline make-state state-pos state-vel state-acc state-mass))
-(declaim (inline dist))
 
 (defmacro state (x y z vx vy vz m)
   `(make-state :pos (v3 ,x ,y ,z) :vel (v3 ,vx ,vy ,vz) :mass ,m))
